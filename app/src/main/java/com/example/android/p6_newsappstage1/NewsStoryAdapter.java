35ccde9ea2baff3102ae837f33502a65361d9597
Cristina Sterea <ctomacristina@yahoo.com>
2018-04-17 22:42:25 +0200

    Implemented ViewHolder

--- app/src/main/java/com/example/android/p6_newsappstage1/NewsStoryAdapter.java
+++ app/src/main/java/com/example/android/p6_newsappstage1/NewsStoryAdapter.java
@@ -16,6 +16,15 @@ import java.util.Locale;
 
 public class NewsStoryAdapter extends ArrayAdapter<NewsStory> {
 
+    private class ViewHolder {
+        public TextView titleView, sectionNameView, contributorNameView, dateView;
+    }
+
+    // Date formats used to build the date strings.
+    private static SimpleDateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US);
+    //input.setTimeZone(TimeZone.getTimeZone("UTC"));
+    private static SimpleDateFormat outputDateFormat = new SimpleDateFormat("yyyy-MM-dd  HH:mm", Locale.US);
+
     /**
      * Create a new {@link NewsStoryAdapter} object.
      *
@@ -32,76 +41,59 @@ public class NewsStoryAdapter extends ArrayAdapter<NewsStory> {
      * based on a data source, which is a list of {@link NewsStory} objects.
      */
 
+
     @NonNull
     @Override
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+
+        ViewHolder holder;
+
         // Check if an existing list item view (called convertView) that is being reused,
         // otherwise inflate a new list item
         View listItemView = convertView;
         if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.news_story_list_item, parent, false);
+
+            //This is the first time the function is called for this item.
+            //Create and save the view holder as the "tag".
+            holder = new ViewHolder();
+            holder.titleView = listItemView.findViewById(R.id.title);
+            holder.sectionNameView = listItemView.findViewById(R.id.section_name);
+            holder.contributorNameView = listItemView.findViewById(R.id.contributor);
+            holder.dateView = listItemView.findViewById(R.id.date);
+
+            listItemView.setTag(holder);
+        } else {
+            //Get the already saved tag object.
+            holder = (ViewHolder)listItemView.getTag();
         }
 
         // Get the {@link NewsStory} object located at this position in the list
         NewsStory currentNewsStory = getItem(position);
 
-        // Find the TextView with view ID title
-        TextView titleView = listItemView.findViewById(R.id.title);
         // Get the title
-        String title = null;
         if (currentNewsStory != null) {
-            title = currentNewsStory.getTitle();
+            // Display the title of the current news story in that TextView
+            holder.titleView.setText(currentNewsStory.getTitle());
+            //Display the section name of the current news story in that TextView
+            holder.sectionNameView.setText(currentNewsStory.getSectionName());
+            //Display the contributor name of the current news story in that TextView
+            holder.contributorNameView.setText(currentNewsStory.getContributorName());
+
+            //Parse the String which holds the date and time (original "2018-04-15T08:35:35Z" to
+            //"2018-04-15" and "08:35:35", and from "08:35:35" to "08:35")
+            String originalDate = currentNewsStory.getWebPublicationDate();
+            try {
+                Date d = inputDateFormat.parse(originalDate);
+                String formattedDateTime = outputDateFormat.format(d);
+
+                // Display the date of the current news story in that TextView
+                holder.dateView.setText(formattedDateTime);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
         }
-        // Display the title of the current news story in that TextView
-        titleView.setText(title);
-
-        // Get the section name string from the NewsStory object
-        String sectionName = null;
-        if (currentNewsStory != null) {
-            sectionName = currentNewsStory.getSectionName();
-        }
-        // Find the TextView in the news_story_list_item.xml layout with the ID location_offset.
-        TextView sectionNameView = listItemView.findViewById(R.id.section_name);
-        //Display the section name of the current news story in that TextView
-        sectionNameView.setText(sectionName);
-
-        // Get the contributor's name string from the NewsStory object
-        String contributorName = null;
-        if (currentNewsStory != null) {
-            contributorName = currentNewsStory.getContributorName();
-        }
-        // Find the TextView in the news_story_list_item.xml layout with the ID location_offset.
-        TextView contributorNameView = listItemView.findViewById(R.id.contributor);
-        //Display the section name of the current news story in that TextView
-        contributorNameView.setText(contributorName);
-
-
-        //Parse the String which holds the date and time (original "2018-04-15T08:35:35Z" to
-        //"2018-04-15" and "08:35:35", and from "08:35:35" to "08:35")
-        String originalDate = null;
-
-        if (currentNewsStory != null) {
-            originalDate = currentNewsStory.getWebPublicationDate();
-        }
-
-        String formattedDateTime = null;
-        SimpleDateFormat input = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US);
-        //input.setTimeZone(TimeZone.getTimeZone("UTC"));
-        SimpleDateFormat output = new SimpleDateFormat("yyyy-MM-dd  HH:mm", Locale.US);
-
-        Date d;
-        try {
-            d = input.parse(originalDate);
-            formattedDateTime = output.format(d);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        // Find the TextView in the news_story_list_item.xml layout with the ID date.
-        TextView dateView = listItemView.findViewById(R.id.date);
-        // Display the date of the current news story in that TextView
-        dateView.setText(formattedDateTime);
 
         // Return the whole news story list item layout (containing 3 TextViews and a View)
         // so that it can be shown in the ListView.
